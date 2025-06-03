5793224ecfa54b1bbd0bee74df2e677465c79d16
Glen Ernstrom <otterbrain@protonmail.com>
2025-06-03 17:30:58 -0400

    first flatpak install script

---
+++ flatpaks.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sudo flatpak install \
+ com.obsproject.Studio \
+ com.discordapp.Discord \
+ im.riot.Riot \
+ org.gnome.Fractal \
+ org.gnome.Podcasts \
+ org.gnome.World.Citations \
+ org.pymol.PyMOL \
+ org.libreoffice.LibreOffice \
+ org.gimp.GIMP \
+ org.inkscape.Inkscape \
+ net.scribus.Scribus \
+ dev.qwery.AddWater \
+ com.github.jeromerobert.pdfarranger \
+ org.kde.kdenlive \
+ org.videolan.VLC \
+ com.logseq.Logseq \
+ io.gitlab.news_flash.NewsFlash \
+ com.github.johnfactotum.Foliate \
+ com.calibre_ebook.calibre \
+ com.rafaelmardojai.Blanket \
+ org.gnome.Solanum \
+ io.gitlab.adhami3310.Converter \
+ com.github.finefindus.eyedropper \
+ com.github.flxzt.rnote \
+ com.github.xournalpp.xournalpp \
+ io.github.david_swift.Flashcards \
+ com.mattjakeman.ExtensionManager \
